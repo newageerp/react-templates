310c687f2447c224fee2be0548592cecf1f11cd6
Arturas Jegorenkovas <jegorenkov@gmail.com>
2022-09-12 10:00:26 +0000

    updates

---
+++ CoreTemplates/Edit/EditContent.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Newageerp\SfReactTemplates\CoreTemplates\Edit;
+
+use Newageerp\SfPermissions\Service\EntityPermissionService;
+use Newageerp\SfReactTemplates\Template\Template;
+
+class EditContent extends Template
+{
+    protected string $schema = '';
+
+    protected string $type = '';
+
+    protected string $id = '';
+
+    protected ?object $entity = null;
+
+    protected ?int $defaultViewIndex = null;
+
+    public function __construct(string $schema, string $type, string $id, ?object $entity)
+    {
+        $this->schema = $schema;
+        $this->type = $type;
+        $this->id = $id;
+        $this->entity = $entity;
+    }
+
+    public function getTemplateData(): array
+    {
+        return [
+            
+        ];
+    }
+
+    public function getProps(): array
+    {
+        return [
+            'schema' => $this->getSchema(),
+            'type' => $this->getType(),
+            'id' => $this->getId(),
+            'defaultViewIndex' => $this->getDefaultViewIndex(),
+        ];
+    }
+
+    public function getTemplateName(): string
+    {
+        return 'edit.content';
+    }
+
+    /**
+     * Get the value of schema
+     *
+     * @return string
+     */
+    public function getSchema(): string
+    {
+        return $this->schema;
+    }
+
+    /**
+     * Set the value of schema
+     *
+     * @param string $schema
+     *
+     * @return self
+     */
+    public function setSchema(string $schema): self
+    {
+        $this->schema = $schema;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of type
+     *
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set the value of type
+     *
+     * @param string $type
+     *
+     * @return self
+     */
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id
+     *
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @param string $id
+     *
+     * @return self
+     */
+    public function setId(string $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of defaultViewIndex
+     *
+     * @return ?int
+     */
+    public function getDefaultViewIndex(): ?int
+    {
+        return $this->defaultViewIndex;
+    }
+
+    /**
+     * Set the value of defaultViewIndex
+     *
+     * @param ?int $defaultViewIndex
+     *
+     * @return self
+     */
+    public function setDefaultViewIndex(?int $defaultViewIndex): self
+    {
+        $this->defaultViewIndex = $defaultViewIndex;
+
+        return $this;
+    }
+}
---
+++ CoreTemplates/Edit/EditContentListener.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Newageerp\SfReactTemplates\CoreTemplates\Edit;
+
+use Newageerp\SfControlpanel\Console\EntitiesUtilsV3;
+use Newageerp\SfReactTemplates\CoreTemplates\Popup\PopupWindow;
+use Newageerp\SfReactTemplates\CoreTemplates\Toolbar\ToolbarTitle;
+use Newageerp\SfReactTemplates\Event\LoadTemplateEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Newageerp\SfUservice\Service\UService;
+
+class EditContentListener implements EventSubscriberInterface
+{
+    protected UService $uservice;
+
+    protected EntitiesUtilsV3 $entitiesUtilsV3;
+
+    public function __construct(UService $uservice, EntitiesUtilsV3 $entitiesUtilsV3)
+    {
+        $this->uservice = $uservice;
+        $this->entitiesUtilsV3 = $entitiesUtilsV3;
+    }
+
+    public function onTemplate(LoadTemplateEvent $event)
+    {
+        if ($event->isTemplateForAnyEntity('PageMainEdit')) {
+            $entity = $this->uservice->getEntityFromSchemaAndId(
+                $event->getData()['schema'],
+                $event->getData()['id']
+            );
+            $editContent = new EditContent(
+                $event->getData()['schema'],
+                $event->getData()['type'],
+                $event->getData()['id'],
+                $entity
+            );
+            $isPopup = isset($event->getData()['isPopup']) && $event->getData()['isPopup'];
+
+            if ($isPopup) {
+                $popupWindow = new PopupWindow();
+                $popupWindow->getChildren()->addTemplate($editContent);
+                $event->getPlaceholder()->addTemplate($popupWindow);
+            } else {
+                $event->getPlaceholder()->addTemplate($editContent);
+
+                $toolbarTitle = new ToolbarTitle($this->entitiesUtilsV3->getTitleBySlug($event->getData()['schema']));
+                $event->getPlaceholder()->addTemplate($toolbarTitle);
+            }
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            LoadTemplateEvent::NAME => 'onTemplate'
+        ];
+    }
+}
