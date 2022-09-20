e297df6a07ebcece02d4c3c90ba9cbee702ea880
Arturas Jegorenkovas <jegorenkov@gmail.com>
2022-09-20 16:15:29 +0000

    updates

--- CoreTemplates/Form/EditableFields/EnumMultiTextEditableField.php
+++ CoreTemplates/Form/EditableFields/EnumMultiTextEditableField.php
@@ -8,9 +8,19 @@ use Newageerp\SfReactTemplates\Template\Template;
 
 class EnumMultiTextEditableField extends FormBaseField
 {
+    protected array $optisons = [];
+
+    public function __construct(string $key, array $options)
+    {
+        parent::__construct($key);
+        $this->options = $options;
+    }
+    
     public function getProps(): array
     {
         $props = parent::getProps();
+
+        $props['options'] = $this->getOptions();
 
         return $props;
     }
@@ -18,5 +28,29 @@ class EnumMultiTextEditableField extends FormBaseField
     public function getTemplateName(): string
     {
         return 'form.editable.enummultitextfield';
+    }
+
+    /**
+     * Get the value of options
+     *
+     * @return array
+     */
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+
+    /**
+     * Set the value of options
+     *
+     * @param array $options
+     *
+     * @return self
+     */
+    public function setOptions(array $options): self
+    {
+        $this->options = $options;
+
+        return $this;
     }
 }
