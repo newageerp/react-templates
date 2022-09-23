ff4febfef28d48cc60a66c0475d73f641805aca2
Arturas Jegorenkovas <jegorenkov@gmail.com>
2022-09-23 06:47:46 +0000

    updates

--- CoreTemplates/CustomPluginTemplate.php
+++ CoreTemplates/CustomPluginTemplate.php
@@ -2,17 +2,20 @@
 
 namespace Newageerp\SfReactTemplates\CoreTemplates;
 
+use Newageerp\SfReactTemplates\Template\Placeholder;
 use Newageerp\SfReactTemplates\Template\Template;
 
 class CustomPluginTemplate extends Template
 {
     protected string $templateName = '';
     protected array $props = [];
+    protected Placeholder $children;
 
     public function __construct(string $templateName, array $props)
     {
         $this->templateName = $templateName;
         $this->props = $props;
+        $this->children = new Placeholder();
     }
 
     /**
@@ -59,6 +62,30 @@ class CustomPluginTemplate extends Template
     public function setProps(array $props): self
     {
         $this->props = $props;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of children
+     *
+     * @return Placeholder
+     */
+    public function getChildren(): Placeholder
+    {
+        return $this->children;
+    }
+
+    /**
+     * Set the value of children
+     *
+     * @param Placeholder $children
+     *
+     * @return self
+     */
+    public function setChildren(Placeholder $children): self
+    {
+        $this->children = $children;
 
         return $this;
     }
